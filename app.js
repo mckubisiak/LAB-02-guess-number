0eac91702a2e87f00e41660ebc98a9787f2300bd
Kubi Kubisiak <mckubisiak@gmail.com>
2021-05-04 16:36:23 -0700

    create intial html framework and added ids

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // import functions and grab DOM elements
+const guessButton = document.getElementById ('guess-button');
+const resultsDiv = document.getElementById ('results');
+const userGuess = document.getElementById ('user-guess');
 
+//console.log(guessButton);
+//console.log(resultsDiv);
+//console.log(userGuess);
 // initialize state
+
 
 // set event listeners to update state and DOM
