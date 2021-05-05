2252c14f3b4a5cad25059c0a6973e7216fb26418
Kubi Kubisiak <mckubisiak@gmail.com>
2021-05-05 10:22:59 -0700

    completed test function added function & javascript

--- utils.js
+++ utils.js
@@ -1,9 +1,23 @@
+//export { checkForMatch } from '../utils.js';
 
 
+export function checkForMatch(userinput, randomnumber) {
+    if(userinput > randomnumber){
+        return 1;
+    }
+    if(userinput < randomnumber){
+        return -1;
+    }
+    if(userinput === randomnumber){
+        return 0;
+    }
 
-if {
-    userGuess is equal to random# 
-    then increase win counter increase
-    display win
-    else display lose 
 }
+
+
+// if {
+//     userGuess is equal to random# 
+//     then increase win counter increase
+//     display win
+//     else display lose 
+// }
