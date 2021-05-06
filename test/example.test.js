682a8850f0e4de8a0120bcffc1e1149f6dafe6ff
Kubi Kubisiak <mckubisiak@gmail.com>
2021-05-05 18:30:39 -0700

    updated import

--- test/example.test.js
+++ test/example.test.js
@@ -1,17 +1,34 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+
+import { checkForMatch } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
+test('check if user in put is lower than correct number and returns -1', (expect) => {
+    const expected = -1 ;  
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
-
+    
+    const actual = checkForMatch(1, 2);
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('check if user input matches the correct number and returns 0', (expect) => {
+    const expected = 0;  
+    //Act 
+    // Call the function you're testing and set the result to a const
+    
+    const actual = checkForMatch(2, 2);
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('check if user in put is higher than correct number and returns 1', (expect) => {
+    const expected = 1 ;  
+    //Act 
+    // Call the function you're testing and set the result to a const
+    
+    const actual = checkForMatch(2, 1);
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
